6cd8e20dee8632cfedf6d90e2f0238d8d258e23c
seba <sebastian.fiecko@gmail.com>
2018-01-08 19:03:29 +0100

    RemoveWorker functionality

--- src/com/ericsson/nrgsdk/examples/applications/whereami/Service.java
+++ src/com/ericsson/nrgsdk/examples/applications/whereami/Service.java
@@ -5,28 +5,36 @@ import java.util.ArrayList;
 import com.ericsson.nrgsdk.examples.applications.whereami.Configuration;
 import com.ericsson.nrgsdk.examples.applications.whereami.Feature;
 
-public class Service 
-{
+public class Service {
 	private Feature parent;
 	private ArrayList<Worker> usersOfService;
-	
-	public Service(Feature parent)
-	{
+
+	public Service(Feature parent) {
 		this.parent = parent;
-		
+
 		usersOfService = new ArrayList<Worker>();
 	}
-	
-	public boolean addWorker(Worker worker)
-	{
-		if (!usersOfService.contains(worker))
-		{
+
+	public boolean addWorker(Worker worker) {
+		if (!usersOfService.contains(worker)) {
 			usersOfService.add(worker);
 			System.out.println("Dodano uzytkownika o numerze " + worker.getNumer());
 			return true;
 		}
-		
+
 		return false;
+	}
+
+	public boolean removeWorker(Worker worker)
+	{
+		if(!usersOfService.contains(worker)){
+			System.out.println("Blad! Nie ma takiego uzytkownika! :( " + worker.getNumer());
+			return false;
+		}
+		else
+			usersOfService.remove(worker);
+			return true;
+
 	}
 	
 	public ArrayList<Worker> getUserOfService() {
